b329f7c7a9f31ca074a198a9a204ad59588d394b
LRDN <git@lrdn.net>
2022-08-21 14:02:52 +0200

    Add global styles

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import type { FC } from 'react'
-
-const App: FC = () => {
-  return <></>
-}
-
-export default App
---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import '@fontsource/inter/latin-400.css'
+import '@fontsource/inter/latin-700.css'
+import 'normalize.css/normalize.css'
+import '@styles/styles.scss'
+
+import type { FC } from 'react'
+
+const App: FC = () => {
+  return <></>
+}
+
+export default App
