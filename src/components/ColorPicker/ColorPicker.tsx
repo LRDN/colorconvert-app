5805303c50d023c07e80505807173ba0a26f5864
LRDN <git@lrdn.net>
2022-09-05 22:25:31 +0200

    Add color picker elements

--- src/components/ColorPicker/ColorPicker.tsx
+++ src/components/ColorPicker/ColorPicker.tsx
@@ -1,8 +1,33 @@
 import type { FC } from 'react'
+import { colord } from 'colord'
+import { useContext } from 'react'
+import Transparency from '@components/Transparency'
+import { ColorContext } from '@context/ColorContext'
+import ColorAnalysis from '@components/ColorAnalysis'
 import styles from './ColorPicker.module.scss'
 
 const ColorPicker: FC = () => {
-  return <div className={styles.colorPicker}></div>
+  const { colors, activeColor } = useContext(ColorContext)
+  const hsvColor = colord(colors[activeColor]).alpha(1).toHsv()
+  const valueSliderColor = colord({ ...hsvColor, v: 100 }).toHex()
+  const alphaSliderColor = colord(hsvColor).toHex()
+
+  return (
+    <div className={styles.colorPicker}>
+      <div className={styles.colorPicker__wheel}></div>
+      <div
+        className={styles.colorPicker__slider}
+        style={{ color: valueSliderColor }}
+      ></div>
+      <div
+        className={styles.colorPicker__slider}
+        style={{ color: alphaSliderColor }}
+      >
+        <Transparency />
+      </div>
+      <ColorAnalysis className={styles.colorPicker__analysis} />
+    </div>
+  )
 }
 
 export default ColorPicker
