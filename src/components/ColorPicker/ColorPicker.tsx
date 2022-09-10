b82fad19fe26350aa6c10cea2ff823eb73a027db
LRDN <git@lrdn.net>
2022-09-10 17:36:17 +0200

    Add handles to color picker

--- src/components/ColorPicker/ColorPicker.tsx
+++ src/components/ColorPicker/ColorPicker.tsx
@@ -8,21 +8,44 @@ import styles from './ColorPicker.module.scss'
 
 const ColorPicker: FC = () => {
   const { colors, activeColor } = useContext(ColorContext)
-  const hsvColor = colord(colors[activeColor]).alpha(1).toHsv()
-  const valueSliderColor = colord({ ...hsvColor, v: 100 }).toHex()
-  const alphaSliderColor = colord(hsvColor).toHex()
+  const { h, s, v, a } = colord(colors[activeColor]).toHsv()
+
+  const hue = {
+    handleStyle: {
+      color: colord({ h, s, v: 100, a: 1 }).toHex(),
+      top: 50 + (s / 2) * Math.sin((h - 90) * (Math.PI / 180)) + '%',
+      left: 50 + (s / 2) * Math.cos((h - 90) * (Math.PI / 180)) + '%',
+    },
+  }
+
+  const value = {
+    sliderStyle: { color: colord({ h, s, v: 100, a: 1 }).toHex() },
+    handleStyle: {
+      color: colord({ h, s, v, a: 1 }).toHex(),
+      top: 100 - v + '%',
+    },
+  }
+
+  const alpha = {
+    sliderStyle: { color: colord({ h, s, v, a: 1 }).toHex() },
+    handleStyle: {
+      color: colord({ h, s, v, a }).toHex(),
+      top: 100 - a * 100 + '%',
+    },
+  }
 
   return (
     <div className={styles.colorPicker}>
-      <div className={styles.colorPicker__wheel}></div>
-      <div
-        className={styles.colorPicker__slider}
-        style={{ color: valueSliderColor }}
-      ></div>
-      <div
-        className={styles.colorPicker__slider}
-        style={{ color: alphaSliderColor }}
-      >
+      <div className={styles.colorPicker__wheel}>
+        <div className={styles.colorPicker__handle} style={hue.handleStyle} />
+      </div>
+      <div className={styles.colorPicker__slider} style={value.sliderStyle}>
+        <div className={styles.colorPicker__handle} style={value.handleStyle} />
+      </div>
+      <div className={styles.colorPicker__slider} style={alpha.sliderStyle}>
+        <div className={styles.colorPicker__handle} style={alpha.handleStyle}>
+          <Transparency />
+        </div>
         <Transparency />
       </div>
       <ColorAnalysis className={styles.colorPicker__analysis} />
